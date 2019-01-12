7ea6b5a3fc59996b855696ed766de7844b997d22
Aaron Griffith <aargri@gmail.com>
2019-01-12 23:59:28 +0000

    syntax highlighting for log view

--- logviewer.py
+++ logviewer.py
@@ -1,7 +1,95 @@
 import znc
+import html
+import re
 import os
 import os.path
 import urllib.parse
+
+# regexes taken from pygments, under a BSD 2-clause license
+# https://bitbucket.org/birkenfeld/pygments-main/src/default/LICENSE
+class Colorizer:
+    timestamp = r"""
+        (
+          # irssi / xchat and others
+          (?: \[|\()?                  # Opening bracket or paren for the timestamp
+            (?:                        # Timestamp
+                (?: (?:\d{1,4} [-/])*  # Date as - or /-separated groups of digits
+                    (?:\d{1,4})
+                 [T ])?                # Date/time separator: T or space
+                (?: \d?\d [:.])*       # Time as :/.-separated groups of 1 or 2 digits
+                    (?: \d?\d)
+            )
+          (?: \]|\))?\s+               # Closing bracket or paren for the timestamp
+        )?
+    """
+    # (regex, [wholeclass, groupclasses, ...])
+    matchersrc = [
+        # log start/end
+        (r'^\*\*\*\*(.*)\*\*\*\*$', ['muted', None]),
+        # hack
+        ('^' + timestamp + r'(\s*<([^>]*)>\s*)$', [None, 'timestamp', 'nickname-bracket', 'nickname']),
+        # normal messages
+        ('^' + timestamp + r"""
+            (\s*<(.*?)>\s*)            # Nick-bracket and Nick
+            (?: (\S+):(?!//))?         # Prefix
+            .*$                        # rest of message
+        """, [None, 'timestamp', 'nickname-bracket', 'nickname', 'prefix']),
+        # /me messages
+        ('^' + timestamp + r"""
+            (\s*[*]\s+)                # Star
+            (\S+\s+)                   # Nick
+            .*$                        # rest of message
+        """, [None, 'timestamp', 'keyword', 'nickname']),
+        # join / part messages
+        ('^' + timestamp + r"""
+            (\s*(?:\*{3}|<?-[!@=P]?->?)\s*)  # Star(s) or symbols
+            (.*)$                      # rest of message
+        """, [None, 'timestamp', 'keyword', 'muted']),
+        # catchall with timestamp
+        ('^' + timestamp + '.*$', [None, 'timestamp']),
+    ]
+
+    matchers = []
+    for resrc, groups in matchersrc:
+        matchers.append((re.compile(resrc, re.X | re.M), groups))
+
+    def colorize(self, line):
+        for i, (r, groups) in enumerate(self.matchers):
+            res = r.match(line)
+            if not res:
+                continue
+
+            return [(cls, res.span(i)) for i, cls in enumerate(groups) if cls]
+        return []
+
+    def chunkify(self, line, spans):
+        keyidx = set([0, len(line)])
+        for _, (start, end) in spans:
+            if start < 0 or end < 0:
+                continue
+            keyidx.add(start)
+            keyidx.add(end)
+        keyidx = list(sorted(keyidx))
+        chunks = []
+        for i in range(len(keyidx) - 1):
+            start = keyidx[i]
+            end = keyidx[i + 1]
+            cls = []
+            for testcls, (clsstart, clsend) in spans:
+                if clsstart <= start and end <= clsend:
+                    cls.append(testcls)
+            chunks.append((cls, line[start:end]))
+
+        return chunks
+
+    def make_html(self, chunks, prefix='log-'):
+        gather = ''
+        for cls, chunk in chunks:
+            if cls:
+                gather += '<span class="{0}">{1}</span>'.format(' '.join([prefix + c for c in cls]), html.escape(chunk))
+            else:
+                gather += html.escape(chunk)
+        return gather
 
 class logviewer(znc.Module):
     module_types = [
@@ -78,10 +166,36 @@ class logviewer(znc.Module):
         row = tmpl.AddRow('LogFormats')
         row['url'] = self.get_url('raw', {'path': rel})
         row['name'] = 'Raw Text'
+        color = Colorizer()
         with open(path, encoding='utf-8', errors='replace') as f:
             for i, line in enumerate(f.readlines()):
                 row = tmpl.AddRow('LogLines')
-                row['line'] = line
+                line = line.strip()
+                spans = color.colorize(line)
+                chunks = color.chunkify(line, spans)
+                timestamp = []
+                nickname = []
+                rest = []
+                in_other = False
+                # do nickname colors, and also partition into
+                # timestamp, nickname, rest
+                for cls, chunk in chunks:
+                    if 'nickname' in cls or 'prefix' in cls:
+                        namecolor = hash(chunk.strip()) % 10
+                        cls.append('nickname-{0}'.format(namecolor))
+                    if in_other:
+                        rest.append((cls, chunk))
+                    else:
+                        if 'nickname-bracket' in cls or 'keyword' in cls or 'nickname' in cls:
+                            nickname.append((cls, chunk))
+                        elif 'timestamp' in cls:
+                            timestamp.append((cls, chunk))
+                        else:
+                            in_other = True
+                            rest.append((cls, chunk))
+                row['timestamp'] = color.make_html(timestamp)
+                row['nickname'] = color.make_html(nickname)
+                row['message'] = color.make_html(rest)
                 row['lineno'] = str(i + 1)
 
         return True
