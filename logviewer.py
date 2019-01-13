d51e894bdf707fbedfa4cef245717706d042e999
Aaron Griffith <aargri@gmail.com>
2019-01-13 02:39:14 +0000

    initial functionality for search

--- logviewer.py
+++ logviewer.py
@@ -128,6 +128,8 @@ class logviewer(znc.Module):
             return self.render_dir(sock, tmpl)
         elif page == 'log':
             return self.render_log(sock, tmpl)
+        elif page == 'search':
+            return self.render_search(sock, tmpl)
         else:
             sock.PrintErrorPage(404, "Not Found", "unknown page")
             return False
@@ -139,18 +141,15 @@ class logviewer(znc.Module):
 
         self.render_breadcrumbs(tmpl, rel, dir=True)
 
-        for name in sorted(os.listdir(path)):
-            subpath = os.path.join(path, name)
-            subrel = os.path.join(rel, name)
-            
+        tmpl['path'] = rel
+        for subpath, subrel, name, typ in self.walk_safe_path(path, rel):
             row = tmpl.AddRow('FileList')
             row['name'] = name
-            if os.path.isfile(subpath):
-                row['type'] = 'file'
+            row['type'] = typ
+            if typ == 'file':
                 row['url'] = self.get_url('log', {'path': subrel})
                 row['size'] = self.pretty_number(os.path.getsize(subpath), 'B', True)
-            elif os.path.isdir(subpath):
-                row['type'] = 'dir'
+            elif typ == 'dir':
                 row['url'] = self.get_url('index', {'path': subrel})
                 row['size'] = self.pretty_number(len(os.listdir(subpath)))
 
@@ -166,39 +165,81 @@ class logviewer(znc.Module):
         row = tmpl.AddRow('LogFormats')
         row['url'] = self.get_url('raw', {'path': rel})
         row['name'] = 'Raw Text'
-        color = Colorizer()
+
         with open(path, encoding='utf-8', errors='replace') as f:
-            for i, line in enumerate(f.readlines()):
-                row = tmpl.AddRow('LogLines')
-                line = line.strip()
-                spans = color.colorize(line)
-                chunks = color.chunkify(line, spans)
-                timestamp = []
-                nickname = []
-                rest = []
-                in_other = False
-                # do nickname colors, and also partition into
-                # timestamp, nickname, rest
-                for cls, chunk in chunks:
-                    if 'nickname' in cls or 'prefix' in cls:
-                        namecolor = hash(chunk.strip()) % 10
-                        cls.append('nickname-{0}'.format(namecolor))
-                    if in_other:
-                        rest.append((cls, chunk))
-                    else:
-                        if 'nickname-bracket' in cls or 'keyword' in cls or 'nickname' in cls:
-                            nickname.append((cls, chunk))
-                        elif 'timestamp' in cls:
-                            timestamp.append((cls, chunk))
-                        else:
-                            in_other = True
-                            rest.append((cls, chunk))
-                row['timestamp'] = color.make_html(timestamp)
-                row['nickname'] = color.make_html(nickname)
-                row['message'] = color.make_html(rest)
-                row['lineno'] = str(i + 1)
+            self.render_loglines(tmpl, rel, f.readlines())
 
         return True
+
+    def render_search(self, sock, tmpl):
+        path, rel = self.get_safe_path(sock, dir=True)
+        if not path:
+            return False
+
+        self.render_breadcrumbs(tmpl, rel, dir=True, last=False)
+
+        regexsrc = sock.GetParam('re', False)
+        tmpl['path'] = rel
+        tmpl['re'] = regexsrc
+
+        try:
+            regex = re.compile(regexsrc, re.M)
+            if regex.match(''):
+                raise RuntimeError('search matches empty string')
+        except Exception as e:
+            tmpl['error'] = str(e)
+            return True
+
+        startfile = None
+        startline = None
+        results = 0
+        for subpath, subrel, name, typ in self.walk_safe_path(path, rel, recurse=True):
+            if typ == 'dir':
+                continue
+            if startfile and subrel != startfile:
+                continue
+            startfile = None
+
+            with open(subpath, encoding='utf-8', errors='replace') as f:
+                for lines in self.grep(f, regex, startline):
+                    row = tmpl.AddRow('LogFiles')
+                    self.render_breadcrumbs(row, subrel, last=False, frag='#L{0}'.format(lines[0][0] + 1))
+                    self.render_loglines(row, subrel, lines, simple=False)
+                    results += 1
+                    if results >= 30:
+                        return True
+
+        return True
+
+    def grep(self, f, regex, startline=None, context=3):
+        context_pre = []
+        found = []
+        end_context_left = 0
+        for i, line in enumerate(f.readlines()):
+            context_pre[:] = context_pre[-context:]
+            context_pre.append((i, line, False, []))
+            if startline and i < startline - 1:
+                continue
+
+            r = regex.search(line)
+            if not r:
+                if end_context_left > 0:
+                    found.append((i, line, False, []))
+                    end_context_left -= 1
+                if found and end_context_left == 0:
+                    yield found
+                    found = []
+                continue
+
+            spans = [('searchword', m.span(0)) for m in regex.finditer(line)]
+            if found:
+                found.append((i, line, True, spans))
+            else:
+                found = context_pre[:]
+                found[-1] = (i, line, True, spans)
+            end_context_left = context
+        if found:
+            yield found
 
     def render_raw(self, sock):
         path, rel = self.get_safe_path(sock)
@@ -207,22 +248,62 @@ class logviewer(znc.Module):
         sock.PrintFile(path, 'text/plain')
         return True
 
-    def render_breadcrumbs(self, tmpl, rel, dir=False):
-        parts = [p for p in os.path.split(rel) if p]
+    def render_loglines(self, tmpl, rel, lines, simple=True):
+        fulllines = lines
+        if simple:
+            fulllines = ((i, line, False, []) for i, line in enumerate(lines))
+
+        color = Colorizer()
+        for i, line, highlight, extraspan in fulllines:
+            row = tmpl.AddRow('LogLines')
+            row['highlight'] = 'true' if highlight else 'false'
+            line = line.strip()
+            spans = color.colorize(line) + extraspan
+            chunks = color.chunkify(line, spans)
+            timestamp = []
+            nickname = []
+            rest = []
+            in_other = False
+            # do nickname colors, and also partition into
+            # timestamp, nickname, rest
+            for cls, chunk in chunks:
+                if 'nickname' in cls or 'prefix' in cls:
+                    namecolor = hash(chunk.strip()) % 10
+                    cls.append('nickname-{0}'.format(namecolor))
+                if in_other:
+                    rest.append((cls, chunk))
+                else:
+                    if 'nickname-bracket' in cls or 'keyword' in cls or 'nickname' in cls:
+                        nickname.append((cls, chunk))
+                    elif 'timestamp' in cls:
+                        timestamp.append((cls, chunk))
+                    else:
+                        in_other = True
+                        rest.append((cls, chunk))
+            row['timestamp'] = color.make_html(timestamp)
+            row['nickname'] = color.make_html(nickname)
+            row['message'] = color.make_html(rest)
+            row['lineno'] = str(i + 1)
+
+    def render_breadcrumbs(self, tmpl, rel, dir=False, last=True, frag=''):
+        parts = [p for p in rel.split(os.path.sep) if p]
         sofar = ''
         row = tmpl.AddRow('Breadcrumbs')
-        tmpl['breadcrumbs_root'] = './'
+        tmpl['breadcrumbs_root'] = self.get_url('index')
         for i, part in enumerate(parts):
             row = tmpl.AddRow('Breadcrumbs')
             sofar = os.path.join(sofar, part)
-            row['url'] = self.get_url('index', {'path': sofar})
+            onlast = (i == len(parts) - 1)
+            typ = 'dir' if (dir or not onlast) else 'file'
+            row['type'] = typ
+            row['last'] = 'true' if (last and onlast) else 'false'
+            row['url'] = self.get_url('index' if typ == 'dir' else 'log', {'path': sofar}) + (frag if onlast else '')
             row['name'] = part
-            row['type'] = 'dir' if (dir or i < len(parts) - 1) else 'file'
-            row['last'] = 'true' if (i == len(parts) - 1) else 'false'
+
 
     def get_url(self, name, args={}):
         if name == 'index':
-            name = ''
+            name = './'
         if args:
             name += '?' + urllib.parse.urlencode(args)
         return name
@@ -242,6 +323,20 @@ class logviewer(znc.Module):
             s += ' ' + suffix
         return s
 
+    def walk_safe_path(self, rootpath, rootrel, recurse=False):
+        for dirpath, dirnames, filenames in os.walk(rootpath):
+            dirrel = os.path.join(rootrel, self.relpath(dirpath, rootpath))
+            for d in sorted(dirnames):
+                dpath = os.path.join(dirpath, d)
+                drel = os.path.join(dirrel, d)
+                yield dpath, drel, d, 'dir'
+            for f in sorted(filenames, reverse=True):
+                fpath = os.path.join(dirpath, f)
+                frel = os.path.join(dirrel, f)
+                yield fpath, frel, f, 'file'
+            if not recurse:
+                del dirnames[:]
+
     def get_safe_path(self, sock, dir=False):
         path = sock.GetParam('path', False)
         base = os.path.abspath(self.get_log_dir(sock))
@@ -255,10 +350,14 @@ class logviewer(znc.Module):
         if not dir and not os.path.isfile(fullpath):
             sock.PrintErrorPage(404, "Not Found", "not a file")
             return None, None
-        rel = os.path.relpath(fullpath, base)
+        rel = self.relpath(fullpath, base)
+        return fullpath, rel
+
+    def relpath(self, path, base):
+        rel = os.path.relpath(path, base)
         if rel == '.':
             rel = ''
-        return fullpath, rel
+        return rel
 
     def get_log_dir(self, sock):
         base = znc.CZNC.Get().GetZNCPath()
